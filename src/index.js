677b6921ba76a244ea1734fef1d3c67b02106f11
Goggags123 <wongsakorngag@gmail.com>
2020-11-04 04:34:03 +0700

    Fixing bugs

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 app.use(gpsRoute);
-
+app.use(express.static('./public'))
 app.use((req,res,next)=>{
     res.status(404).send('Page not found.');
 })
