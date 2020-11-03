0cd2f4b86835726865dea32a0852bfb25769bf52
Goggags123 <wongsakorngag@gmail.com>
2020-11-04 05:00:04 +0700

    Fixing bugs

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 var express = require('express')
 var path = require('path')
 var serveStatic = require('serve-static')
-var routes = require('./src/index.js')
-
+var router = require('./src/index.js')
+var bodyParser = require('body-parser')
 var app = express()
+
 app.use(serveStatic(path.join(__dirname, 'dist')))
 app.use(bodyParser.json());
-app.use(routes)
+app.use(router)
 app.use((req,res,next)=>{
     res.status(404).send('Page not found.');
 })
