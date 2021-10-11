46e7fbf7d3c7be2bd9fb623a866a7e91284db1c8
wtaweesupmun <wongsakorn.taweesupmunkong@agoda.com>
2021-10-11 21:15:04 +0700

    For testing

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
-var express = require('express')
 var path = require('path')
 var serveStatic = require('serve-static')
 var router = require('./src/index.js')
 var bodyParser = require('body-parser')
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+router.use('/api-docs', swaggerUi.serve);
+router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 var app = express()
+var express = require("express")
+ , url = require("url")
+ , swagger = require("swagger-node-express");
 
+app.use(express.json());
+app.use(express.urlencoded());
+ 
+// Couple the application to the Swagger module.
+swagger.setAppHandler(app);
 app.use(serveStatic(path.join(__dirname, 'dist')))
 app.use(bodyParser.json());
 app.use(router)
@@ -16,4 +27,4 @@ app.use((err,req,res,next)=>{
 })
 var port = process.env.PORT || 8000
 app.listen(port)
-console.log('server started ' + port)
+console.log('server started on' + port)
