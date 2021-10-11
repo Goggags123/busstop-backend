d849e62227dd46272f8c549aec84a47735ee875d
Goggags123 <wongsakorngag@gmail.com>
2021-10-11 21:20:11 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,20 +2,7 @@ var path = require('path')
 var serveStatic = require('serve-static')
 var router = require('./src/index.js')
 var bodyParser = require('body-parser')
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
-router.use('/api-docs', swaggerUi.serve);
-router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 var app = express()
-var express = require("express")
- , url = require("url")
- , swagger = require("swagger-node-express");
-
-app.use(express.json());
-app.use(express.urlencoded());
- 
-// Couple the application to the Swagger module.
-swagger.setAppHandler(app);
 app.use(serveStatic(path.join(__dirname, 'dist')))
 app.use(bodyParser.json());
 app.use(router)
